019b9b8f9109b45ce28f0e2a62bf4345f2956dc5
changemaind90 <maroderx@gmail.com>
2026-09-18 16:38:36 +0300

    refactor login styles

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -42,30 +42,35 @@ export default function LoginPage() {
   };
 
   return (
-    <div style={{ maxWidth: 400, margin: "50px auto" }}>
-      <h1>Вход</h1>
+    <div className="max-w-[400px] mx-auto my-[50px]">
+      <h1 className="text-center mb-1">Вход</h1>
       <form onSubmit={handleSubmit} className="flex flex-col gap-5">
-        <input
-          type="email"
-          placeholder="Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-        />
-        <input
-          type="password"
-          placeholder="Пароль"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-        <button type="submit" disabled={loading}>
-          {loading ? "Вход..." : "Войти"}
+        <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} required
+          className="border-[1px] border-solid" />
+        <input type="password" placeholder="Пароль" value={password} onChange={(e) => setPassword(e.target.value)}
+          className="border-[1px] border-solid"
+          required />
+        <button type="submit" disabled={loading}
+          className="mb-2 group relative w-full flex items-center justify-center rounded-lg bg-[#6366f1] px-1 py-1 
+            text-sm font-medium text-white shadow-md hover:bg-[#4f46ba] active:scale-[0.99] 
+            transition-all duration-200 disabled:opacity-70 disabled:cursor-not-allowed disabled:shadow-none"
+        > {loading ? (
+          <>
+            <span className="animate-spin w-4 h-4 border-2 border-white border-t-transparent rounded-full mr-2"></span>
+            Вход...
+          </>
+        ) : (
+          "Войти"
+        )}
         </button>
         {error && <p className="text-red-500">{error}</p>}
       </form>
-      <p>
-        Нет аккаунта? <Link href="/register">Зарегистрироваться</Link>
+      <p className="text-sm text-[var(--muted-foreground)]">
+        Нет аккаунта?
+        <Link href="/register"
+          className="text-[#6366f1] underline hover:text-[#4f46ba]">
+          Зарегистрироваться
+        </Link>
       </p>
     </div>
   );
